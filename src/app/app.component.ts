72ec6af2f2fba8e50fbcba8860389777d325a4d6
2mrcarter <58522066+2mrcarter@users.noreply.github.com>
2020-06-05 12:06:40 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todoList';
+  myName ='Joel Carter';
+
+  constructor(){
+    console.log("Welcome everyone");
+
+    let name = "Joel Carter";
+    let found = false;
+    let list = [];
+   
+  }
+    hello(name){
+      console.log(name);
+    }
+    something(){
+      console.log("Btn clicked");
+      this.title = "Wow, did you see that?!";
+    }
+}
